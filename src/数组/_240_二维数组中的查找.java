0a8f37072fe129989210059db8f78cd845d83e2c
jeatee <diaojt.work@gmail.com>
2021-07-21 01:56:18 +0800

    feat: 剑指offer-数组02-二维数组中的查找+1_leetcode-240

--- src/数组/_240_二维数组中的查找.java
+++ src/数组/_240_二维数组中的查找.java
@@ -1,5 +1,19 @@
 package 数组;
 
 public class _240_二维数组中的查找 {
-
+	public boolean findNumberIn2DArray(int[][] matrix, int target) {
+		// 获取二维数组的行数
+		int i = matrix.length -1;
+		int j = 0;
+		while(i >= 0 && j < matrix[0].length) {
+			if (matrix[i][j] > target) {
+				i--;
+			}else if(matrix[i][j] < target) {
+				j++;
+			}else {
+				return true;
+			}
+		}
+		return false;
+    }
 }
